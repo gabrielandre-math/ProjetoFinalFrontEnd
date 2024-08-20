6d898ad2630893a9bdd0cb8f2a03141abb221cd1
gabrielandré <gabrieldevpyc@gmail.com>
2024-08-20 05:25:09 -0300

    configure: add imports from app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatPaginatorModule } from "@angular/material/paginator"
+import { MatSnackBarModule } from "@angular/material/snack-bar"
+import { MatCheckboxModule } from "@angular/material/checkbox"
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatSidenavModule } from "@angular/material/sidenav"
+import { MatButtonModule } from "@angular/material/button"
+import { MatSelectModule } from "@angular/material/select"
+import { MatTableModule } from "@angular/material/table"
+import { MatRadioModule } from "@angular/material/radio"
+import { MatInputModule } from "@angular/material/input"
+import { MatIconModule } from "@angular/material/icon"
+import { MatListModule } from "@angular/material/list"
+import { MatCardModule } from "@angular/material/card"
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    MatFormFieldModule, MatPaginatorModule, MatSnackBarModule,
+    MatCheckboxModule, MatToolbarModule, MatSidenavModule,
+    MatButtonModule, MatSelectModule, MatTableModule,
+    MatRadioModule, MatInputModule, MatIconModule,
+    MatListModule, MatCardModule, FormsModule,
+    HttpClientModule, ReactiveFormsModule
+  ],
   templateUrl: 'app.component.html',
   styles: [],
 })
