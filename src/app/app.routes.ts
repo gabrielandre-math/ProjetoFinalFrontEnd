7841bd3e819123b4238be9eeceb1b6ca4059bf9c
gabrielandré <gabrieldevpyc@gmail.com>
2024-08-21 02:47:26 -0300

    fix: bugs and errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: NavComponent
+        component: HomeComponent, children: [
+            { path: 'home', component: HomeComponent },
+            { path: 'header', component: HeaderComponent }
+        ]
+
     }
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from '../../header/header.component';
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {
+    this.router.navigate(['home'])
+  }
 }
