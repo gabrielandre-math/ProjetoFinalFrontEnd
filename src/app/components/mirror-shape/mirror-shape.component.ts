69a027a987135088ad4d864e241f65a82297f449
gabrielandré <gabrieldevpyc@gmail.com>
2024-08-21 04:12:30 -0300

    feat: create mirror-shape

---
+++ src/app/components/mirror-shape/mirror-shape.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mirror-shape',
+  standalone: true,
+  imports: [],
+  templateUrl: './mirror-shape.component.html',
+  styleUrl: './mirror-shape.component.css'
+})
+export class MirrorShapeComponent {
+
+}
