cec21a280b9a2f2ff9490c058106202f4268aeeb
gabrielandré <gabrieldevpyc@gmail.com>
2024-08-21 04:10:25 -0300

    feat: create header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+  isMenuOpen = false;
 
+  ngAfterViewInit() {
+    const menuButton = document.getElementById('menu-button');
+    const animatedIcon = document.querySelector('.animated-icon1');
+    const responsiveMenu = document.getElementById('responsive-menu');
+
+    if (menuButton && animatedIcon && responsiveMenu) {
+      menuButton.addEventListener('click', () => {
+        this.isMenuOpen = !this.isMenuOpen;
+        animatedIcon.classList.toggle('open'); // Alterna a animação do ícone
+
+        if (this.isMenuOpen) {
+          // Animação de entrada do menu responsivo
+          responsiveMenu.style.display = 'block';
+          anime({
+            targets: responsiveMenu,
+            height: ['0px', '150px'],
+            opacity: [0, 1],
+            duration: 500,
+            easing: 'easeInOutQuad'
+          });
+        } else {
+          // Animação de saída do menu responsivo
+          anime({
+            targets: responsiveMenu,
+            height: ['150px', '0px'],
+            opacity: [1, 0],
+            duration: 500,
+            easing: 'easeInOutQuad',
+            complete: () => {
+              responsiveMenu.style.display = 'none';
+            }
+          });
+        }
+      });
+    }
+  }
 }
