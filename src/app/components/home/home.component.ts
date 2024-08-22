bb55daca3f54770fa7c69b326640d399adc609ad
gabrielandré <gabrieldevpyc@gmail.com>
2024-08-22 10:42:49 -0300

    configure: some changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
-import { Router } from '@angular/router'
 import { FooterComponent } from '../footer/footer.component';
 import { MirrorShapeComponent } from '../mirror-shape/mirror-shape.component';
-import { FeatureCardsComponent } from '../../feature-cards/feature-cards.component';
+import { FeatureCardsComponent } from '../feature-cards/feature-cards.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [HeaderComponent, FooterComponent, MirrorShapeComponent, FeatureCardsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    this.router.navigate(['home'])
-  }
+  constructor() {}
 }
