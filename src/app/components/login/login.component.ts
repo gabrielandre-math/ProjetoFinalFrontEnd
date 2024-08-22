46cd94b50bf96faff39cc1d293d271607cbe25e0
gabrielandré <gabrieldevpyc@gmail.com>
2024-08-22 17:45:20 -0300

    feat: form validation for login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms'; //ReactiveFormsModule
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ReactiveFormsModule], //ReactiveFormsModule
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  cred: Credenciais = {
+    email: '',
+    senha: ''
+  };
+  email = new FormControl('', [Validators.required, Validators.email]);
+  senha = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
+  
+}
+
+export interface Credenciais {
+  email: string;
+  senha: string;
 }
