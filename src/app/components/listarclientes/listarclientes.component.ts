4355052378cf569090a40b0b659fe0fc94079e18
gabrielandré <gabrieldevpyc@gmail.com>
2024-08-25 19:38:46 -0300

    feat: create listarclientes component

---
+++ src/app/components/listarclientes/listarclientes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listarclientes',
+  standalone: true,
+  imports: [],
+  templateUrl: './listarclientes.component.html',
+  styleUrl: './listarclientes.component.css'
+})
+export class ListarclientesComponent {
+
+}
