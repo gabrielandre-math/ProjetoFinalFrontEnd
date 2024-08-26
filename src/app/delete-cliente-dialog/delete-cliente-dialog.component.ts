b55673c6a7c4ee4da6241bc0681507ed971fc168
gabrielandré <gabrieldevpyc@gmail.com>
2024-08-26 01:07:44 -0300

    feat: create delete-cliente-dialog component

---
+++ src/app/delete-cliente-dialog/delete-cliente-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-cliente-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-cliente-dialog.component.html',
+  styleUrl: './delete-cliente-dialog.component.css'
+})
+export class DeleteClienteDialogComponent {
+
+}
