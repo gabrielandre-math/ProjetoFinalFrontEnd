2cb655a23c36a48478ebe999d427544393ccfe82
gabrielandré <gabrieldevpyc@gmail.com>
2024-08-26 01:33:21 -0300

    feat: create edit-cliente-dialog component

---
+++ src/app/components/edit-cliente-dialog/edit-cliente-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-cliente-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-cliente-dialog.component.html',
+  styleUrl: './edit-cliente-dialog.component.css'
+})
+export class EditClienteDialogComponent {
+
+}
