94796ff54a1096c276329673a33a74a09aa492aa
gabrielandré <gabrieldevpyc@gmail.com>
2024-09-02 21:53:31 -0300

    fix: redirect footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],  // Adicione o CommonModule aqui
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
   isAuthenticated = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.isAuthenticated = this.authService.isAuthenticated();
+  }
+
+  navigateToHome(): void {
+    this.router.navigate(['/home']);
+  }
+
+  navigateToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  navigateToRegister(): void {
+    this.router.navigate(['/register']);
   }
 }
